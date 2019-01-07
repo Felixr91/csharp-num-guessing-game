8047c149cef90efa62d1fac20ef8db1e3b7297f3
Felix <felixr91@gmail.com>
2019-01-07 09:54:13 -0700

    final

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace guessgame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int secret;
+      bool won = false;
+
+
+      Console.WriteLine("Can you guess the secret number?");
+      Random rnd = new Random();
+      secret = rnd.Next(1, 10);
+      Console.WriteLine(secret);
+
+      while (!won)
+      {
+
+        string guessString = Console.ReadLine();
+        int guess;
+        if (Int32.TryParse(guessString, out guess))
+        {
+          if (guess == secret)
+          {
+            Console.WriteLine("You got it, dog!");
+            won = true;
+          }
+          else if (guess > secret)
+          {
+            Console.WriteLine("Guess lower, dog!");
+          }
+          else if (guess < secret)
+          {
+            Console.WriteLine("Gotta guess higher, dog!");
+          }
+        }
+        else
+        {
+          Console.WriteLine("Stop playing! Give me a number!");
+        }
+      }
+    }
+  }
+}
